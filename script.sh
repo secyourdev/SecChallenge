f1f96574637f449b4cce054cfee615d358d4a92f
Kassucre <98214449+Kassucre@users.noreply.github.com>
2023-03-28 12:04:42 -0400

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,80 +1,144 @@
 #!/bin/bash
-mkdir Website/
+NOCOLOR='\033[0m'
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+ORANGE='\033[0;33m'
+BLUE='\033[0;34m'
+PURPLE='\033[0;35m'
+CYAN='\033[0;36m'
+LIGHTGRAY='\033[0;37m'
+DARKGRAY='\033[1;30m'
+LIGHTRED='\033[1;31m'
+LIGHTGREEN='\033[1;32m'
+YELLOW='\033[1;33m'
+LIGHTBLUE='\033[1;34m'
+LIGHTPURPLE='\033[1;35m'
+LIGHTCYAN='\033[1;36m'
+WHITE='\033[1;37m'
+
+
+
+
+
+
+
+while getopts p:b:f flag
+do
+    case "${flag}" in
+      p) port=${OPTARG};;
+      b) branch=${OPTARG};;
+      f) force=true;;
+    esac
+done
+
+echo -e "${CYAN}port: ${port:-80}${NOCOLOR}";
+echo -e "${CYAN}branch: ${branch:-"main"}${NOCOLOR}";
+if [[ "$force" == true ]];then
+  echo -e "${CYAN}force mode${NOCOLOR}";
+fi
+
+if [ ! "$(ls -A Website/)" ];then
+  mkdir Website/
+fi
 cd Website/
-mkdir public_html/
-mkdir mysql/
-mkdir mysql/sql-scripts/
-cat > docker-compose.yml << EOF
-version: "3.7"
-services:
-  php:
-    image: cybersecuriteachg2/php
-    networks:
-      - backend
-    volumes:
-      - \${PROJECT_ROOT}/:/var/www/html/
-    container_name: php
-    links:
-     - mysql
-  apache:
-    image: cybersecuriteachg2/apache
-    depends_on:
-      - php
-      - mysql
-    links:
-      - mysql
-    networks:
-      - frontend
-      - backend
-    ports:
-      - "80:80"
-    volumes:
-      - \${PROJECT_ROOT}/:/var/www/html/
-    container_name: apache
-  mysql:
-    build: ./mysql/
-    command: --default-authentication-plugin=mysql_native_password 
-    restart: always
-    ports:
-      - "3306:3306"
 
-    networks:
-      - backend
-  # The default MySQL installation only creates the "root" administrative account
-  # create new users using docker-compose exec
-    environment:
-      MYSQL_ROOT_PASSWORD: "\${DB_ROOT_PASSWORD}"
-      MYSQL_DATABASE: dbsite
-      MYSQL_USER: "\${DB_USERNAME}"
-      MYSQL_PASSWORD: "\${DB_PASSWORD}"
-    container_name: mysql
+if [ ! "$(ls -A public_html/)" ];then
+  mkdir public_html/
+fi
 
-    
-  phpmyadmin:
-    depends_on:
+if [ ! "$(ls -A mysql/)" ];then
+  mkdir mysql/
+fi
+
+if [ ! "$(ls -A mysql/sql-scripts/)" ];then
+  mkdir mysql/sql-scripts/
+fi
+
+
+if [ "$(ls -A docker-compose.yml)" ] && [[ ! "$force" == true ]];then
+
+  echo "Already have a docker-compose"
+  echo -e "${YELLOW}PORT NOT CHANGE${BLUE} : To change the port, use -p and -f${NOCOLOR}"
+
+else
+  cat > docker-compose.yml << EOF
+  version: "3.7"
+  services:
+    php:
+      image: cybersecuriteachg2/php
+      networks:
+        - backend
+      volumes:
+        - \${PROJECT_ROOT}/:/var/www/html/
+      container_name: php
+      links:
       - mysql
-    links:
-      - mysql
-    image: phpmyadmin/phpmyadmin
-    restart: always
-    ports:
-      - '8081:80'
-    environment:
-      PMA_HOST: mysql
-      MYSQL_USERNAME: "\${DB_USERNAME}"
-      MYSQL_ROOT_PASSWORD: "\${DB_ROOT_PASSWORD}"   
-    networks:
-      - backend
-    volumes:
-      - /sessions
-    container_name: phpmyadmin
-networks:
-  frontend:
-  backend:
-volumes:
-    data:
+    apache:
+      image: cybersecuriteachg2/apache
+      depends_on:
+        - php
+        - mysql
+      links:
+        - mysql
+      networks:
+        - frontend
+        - backend
+      ports:
+        - "${port:-80}:80"
+      volumes:
+        - \${PROJECT_ROOT}/:/var/www/html/
+      container_name: apache
+    mysql:
+      build: ./mysql/
+      command: --default-authentication-plugin=mysql_native_password 
+      restart: always
+      ports:
+        - "3306:3306"
+
+      networks:
+        - backend
+    # The default MySQL installation only creates the "root" administrative account
+    # create new users using docker-compose exec
+      environment:
+        MYSQL_ROOT_PASSWORD: "\${DB_ROOT_PASSWORD}"
+        MYSQL_DATABASE: dbsite
+        MYSQL_USER: "\${DB_USERNAME}"
+        MYSQL_PASSWORD: "\${DB_PASSWORD}"
+      container_name: mysql
+
+      
+    phpmyadmin:
+      depends_on:
+        - mysql
+      links:
+        - mysql
+      image: phpmyadmin/phpmyadmin
+      restart: always
+      ports:
+        - '8081:80'
+      environment:
+        PMA_HOST: mysql
+        MYSQL_USERNAME: "\${DB_USERNAME}"
+        MYSQL_ROOT_PASSWORD: "\${DB_ROOT_PASSWORD}"   
+      networks:
+        - backend
+      volumes:
+        - /sessions
+      container_name: phpmyadmin
+  networks:
+    frontend:
+    backend:
+  volumes:
+      data:
 EOF
+    
+fi
 
+if [ "$(ls -A .env)" ] && [[ ! "$force" == true ]];then
+
+  echo "Already have a .env"
+  
+else
 cat > .env << EOF
 PHP_VERSION=7.4
 MYSQL_VERSION=8
@@ -86,7 +150,13 @@ DB_PASSWORD=root
 
 PROJECT_ROOT=./public_html
 EOF
+fi
 
+if [ "$(ls -A mysql/Dockerfile)" ] && [[ ! "$force" == true ]];then
+
+  echo "Already have a Mysql Dockerfile"
+  
+else
 cat > mysql/Dockerfile << EOF
 FROM mysql/mysql-server:5.7
 
@@ -96,29 +166,39 @@ COPY ./sql-scripts/ /docker-entrypoint-initdb.d/
 COPY ./sql-scripts/init.sql .
 EOF
 
+fi
+
+
 if [ "$(ls -A public_html)" ];then
-    echo "Not empty"
+    
     cd public_html
+    actual=$(git rev-parse --abbrev-ref HEAD);
+    
+    
+
+    if [ "$actual" = "${branch:-"main"}" ]; then
+        echo -e "${GREEN}Already on the good branch${NOCOLOR}"
+    else
+        echo -e "${YELLOW}Changing branch${NOCOLOR}"
+        git checkout ${branch:-"main"}
+    fi
     local=$(git rev-parse HEAD)
     echo $local
-    remote=$(git ls-remote https://github.com/secyourdev/SecChallenge.git 2022_E4_GR2)
+    remote=$(git ls-remote https://github.com/secyourdev/SecChallenge.git ${branch:-"main"})
     remote="${remote:0:40}";
     echo $remote
     if [ "$local" == "$remote" ]; then
         echo "Already up to date"
         cd ..
     else
-        echo "Updating"
-        :'cd ..
-        rm -rf public_html/
-        git clone --branch 2022_E4_GR2 https://github.com/secyourdev/SecChallenge.git public_html/'
+        echo -e "${YELLOW}Updating${NOCOLOR}"
         git pull
         cd ..
     fi
     
     
 else
-    git clone --branch 2022_E4_GR2 https://github.com/secyourdev/SecChallenge.git public_html/
+    git clone --branch ${branch:-"main"} https://github.com/secyourdev/SecChallenge.git public_html/
 fi
 
 cp public_html/bdd/dbsite.sql mysql/sql-scripts/init.sql
@@ -136,4 +216,10 @@ catch(PDOException \$e){
 }
 ?>
 EOF
+
+
+echo -e "${CYAN}Starting dockers${NOCOLOR}"
+
+
+
 sudo docker-compose up --build
