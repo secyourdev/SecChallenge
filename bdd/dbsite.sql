10c01289f08cbd09e9a7cb31842505065f240b7d
lastsurv <103941140+lastsurv@users.noreply.github.com>
2023-04-13 19:42:02 +0200

    Update dbsite.sql

--- bdd/dbsite.sql
+++ bdd/dbsite.sql
@@ -61,7 +61,7 @@ INSERT INTO `challenge` (`IdChallenge`, `Emplacement`, `Reponse`, `NomChallenge`
 (15, 'Challenge/challenge-sqlmap/Challenge-sqlmap.html', '0924086489ca5a44e71715a6c00832cb', 'Sql injection', 'SQL', 'SQLMap', 'PHP', NULL, 5),
 (16, 'Challenge/challenge-cyberchef/challenge-cyberchef.html', '22/12/1973', 'RoDate', 'CyberChef', 'Brute force', NULL, 'Collecte d\'information', 5),
 (17, 'Challenge/Challenge-searchsploit/Challenge-searchsploit.html', 'ESIEE2022', 'Searchsploit', 'scan de vulnérabilité'  , NULL, NULL, 'Scanner de vulnérabilité', 5),
-(18, 'Challenge/Challenge-dirb/challenge-dirb.html', 'CeciEstUnFlag', 'Dirb', 'dirb'  , 'wget', 'cyberchef', 'Collecte d\'information', 5),
+(18, 'Challenge/Challenge-dirb/challenge-dirb.html', 'FLAG_DIRB', 'Dirb', 'dirb'  , 'wget', 'dirb-gendict', 'Collecte d\'information', 5),
 (19, 'Challenge/Challenge-xss/Challenge-xss.html', '4f2cbc87727679862bb4f14a0346fcab', 'Vol de session', 'xss'  , 'dirb', NULL, 'Prise de contôle', 10),
 (20, 'Challenge/Challenge-dnsenum/Challenge-dnsenum.html', '147.20.0.4', 'dns', NULL  , NULL , NULL, 'Collecte d\'information', 5),
 (21, 'Challenge/challenge-hydra/challenge-hydra.html', 'FL4G_Hydra_LeChallenge', 'Hydra', NULL  , NULL , NULL, 'Attaque de mot de passe', 5),
